7cd1eecca60e5ec5137a19a3fbda1b97eb0519e5
eleefulton <eleefulton@gmail.com>
2020-05-28 14:06:02 -0400

    lab3 - simple timer as thread working

--- Fulton_Lab2/src/gameplay/SimpleTimer.java
+++ Fulton_Lab2/src/gameplay/SimpleTimer.java
@@ -2,60 +2,90 @@ package gameplay;
 
 import java.util.ArrayList;
 
-public class SimpleTimer implements Timer {
-  /**
-   * @author Ethan Fulton A class to keep track of the current round and notify
-   *         observers of it
-   */
+public class SimpleTimer extends Thread implements Timer
+{
+	/**
+	 * @author Ethan Fulton A class to keep track of the current round and notify
+	 *         observers of it
+	 */
 
-  private int round;
-  private ArrayList<TimeObserver> theObservers;
+	private int round;
+	private ArrayList<TimeObserver> theObservers;
+	private int waitTime;
 
-  public SimpleTimer() {
-    round = 0;
-    theObservers = new ArrayList<TimeObserver>();
-  }
+	public SimpleTimer()
+	{
+		this(0);
+	}
 
-  /**
-   * @return Return the current round.
-   */
-  public int getRound() {
-    return round;
-  }
+	public SimpleTimer(int i)
+	{
+		round = 0;
+		theObservers = new ArrayList<TimeObserver>();
+		waitTime = i;
+	}
 
-  /**
-   * 
-   * @return Return the list of observers.
-   */
-  public ArrayList<TimeObserver> getObservers() {
-    return theObservers;
-  }
+	/**
+	 * @return Return the current round.
+	 */
+	public int getRound()
+	{
+		return round;
+	}
 
-  /**
-   * add a new observer to the list
-   */
-  @Override
-  public void addTimeObserver(TimeObserver observer) {
-    theObservers.add(observer);
-  }
+	/**
+	 * 
+	 * @return Return the list of observers.
+	 */
+	public ArrayList<TimeObserver> getObservers()
+	{
+		return theObservers;
+	}
 
-  /**
-   * remove an observer from the list
-   */
-  @Override
-  public void removeTimeObserver(TimeObserver observer) {
-    theObservers.remove(observer);
-  }
+	/**
+	 * add a new observer to the list
+	 */
+	@Override
+	public void addTimeObserver(TimeObserver observer)
+	{
+		theObservers.add(observer);
+	}
 
-  /**
-   * change the round and notify observers
-   */
-  @Override
-  public void timeChanged() {
-    round++;
-    for (int i = 0; i < theObservers.size(); i++) {
-      theObservers.get(i).updateTime(round);
-    }
-  }
+	/**
+	 * remove an observer from the list
+	 */
+	@Override
+	public void removeTimeObserver(TimeObserver observer)
+	{
+		theObservers.remove(observer);
+	}
+
+	/**
+	 * change the round and notify observers
+	 */
+	@Override
+	public void timeChanged()
+	{
+		round++;
+		for (int i = 0; i < theObservers.size(); i++)
+		{
+			theObservers.get(i).updateTime(round);
+		}
+	}
+
+	public void run()
+	{
+		while (round < 5)
+		{
+			try
+			{
+				Thread.sleep(waitTime);
+				this.timeChanged();
+			} catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
--- Fulton_Lab2/test/gameplay/TestSimpleTimer.java
+++ Fulton_Lab2/test/gameplay/TestSimpleTimer.java
@@ -4,36 +4,54 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class TestSimpleTimer {
+public class TestSimpleTimer
+{
 
-  @Test
-  public void testInitialization() {
-    SimpleTimer timer = new SimpleTimer();
-    assertEquals(0, timer.getRound());
-    assertEquals(0, timer.getObservers().size());
-    assertTrue(timer instanceof gameplay.Timer);
-  }
-  
-  @Test
-  public void testAddRemoveObserverWithTimeChange() {
-    SimpleTimer timer = new SimpleTimer();
-    MockSimpleTimerObserver observer = new MockSimpleTimerObserver();
-    timer.addTimeObserver(observer);
-    timer.timeChanged();
-    assertEquals(1, observer.myTime);
-    timer.removeTimeObserver(observer);
-    timer.timeChanged();
-    assertEquals(2, timer.getRound());
-    assertEquals(1,  observer.myTime);
-  }
-  
+	/** * This tests that SimpleTimer will update time once * every second. */
+	@Test
+	public void testSimpleTimerAsThread() throws InterruptedException
+	{
+		SimpleTimer st = new SimpleTimer(1000);
+		st.start();
+		Thread.sleep(250); // So we are 1/4th a second different
+		for (int x = 0; x < 5; x++)
+		{
+			assertEquals(x, st.getRound()); // assumes round starts at 0
+			Thread.sleep(1000); // wait for the next time change
+		}
+	}
+
+	@Test
+	public void testInitialization()
+	{
+		SimpleTimer timer = new SimpleTimer();
+		assertEquals(0, timer.getRound());
+		assertEquals(0, timer.getObservers().size());
+		assertTrue(timer instanceof gameplay.Timer);
+	}
+
+	@Test
+	public void testAddRemoveObserverWithTimeChange()
+	{
+		SimpleTimer timer = new SimpleTimer();
+		MockSimpleTimerObserver observer = new MockSimpleTimerObserver();
+		timer.addTimeObserver(observer);
+		timer.timeChanged();
+		assertEquals(1, observer.myTime);
+		timer.removeTimeObserver(observer);
+		timer.timeChanged();
+		assertEquals(2, timer.getRound());
+		assertEquals(1, observer.myTime);
+	}
 
 }
 
-class MockSimpleTimerObserver implements TimeObserver {
-  public int myTime = 0;
+class MockSimpleTimerObserver implements TimeObserver
+{
+	public int myTime = 0;
 
-  public void updateTime(int time) {
-    myTime = time;
-  }
+	public void updateTime(int time)
+	{
+		myTime = time;
+	}
 }
