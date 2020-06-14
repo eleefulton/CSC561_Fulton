db1d521341598c360d791b81339aaad87d084ef7
CASquires <CASquires@MCT060B-074763L.ship.lcl>
2020-06-14 19:37:50 -0400

    grid and test update, lifeform support

--- Fulton_Lab2/src/ui/UI.java
+++ Fulton_Lab2/src/ui/UI.java
@@ -16,6 +16,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import environment.Environment;
+import lifeform.Alien;
+import lifeform.Human;
 import lifeform.LifeForm;
 
 public class UI extends JFrame{
@@ -50,64 +52,45 @@ public class UI extends JFrame{
 		 textButton = new JButton("A Button");
 		 add("East",textButton);
 
-//		 JPanel centerPanel = new JPanel(new GridLayout(theWorld.getNumberOfRows(),theWorld.getNumberOfColumns()));
-//		 JLabel[][] labelArray = new JLabel[theWorld.getNumberOfRows()][theWorld.getNumberOfColumns()];
-//		 for (int r=0;r< theWorld.getNumberOfRows(); r++)
-//		 {
-//		  for (int c=0;c<theWorld.getNumberOfColumns();c++)
-//		  {
-//		  labelArray[r][c] = new JLabel(" ("+r+":"+c+") ");
-//		  centerPanel.add(labelArray[r][c]);
-//		  }
-//		 }
-		 JPanel centerPanel = new JPanel(new GridLayout(10,10));
+		 JPanel centerPanel = new JPanel(new GridLayout(theWorld.getNumberOfRows(),theWorld.getNumberOfColumns()));
 		 centerPanel.setBackground(new Color(255,255,255));
-		 centerPanel.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
+		 //centerPanel.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
+		 JLabel[][] labelArray = new JLabel[theWorld.getNumberOfRows()][theWorld.getNumberOfColumns()];
 		 
-		 JLabel[][] labelArray = new JLabel[10][10];
-		 for (int r=0;r< 10; r++)
+		 for (int r=0;r< theWorld.getNumberOfRows(); r++)
 		 {
-			  for (int c=0;c<10;c++)
-			  {
-				  JLabel label = new JLabel();
-				  label.setBorder(BorderFactory.createLineBorder(new Color(0,0,0)));
-				  centerPanel.add(label);
-				  if (r==0 && c==0)
-				    label.setIcon(createHuman(r,c,"north"));
+			 for (int c=0;c<theWorld.getNumberOfColumns();c++)
+			 {
+				 labelArray[r][c] = new JLabel();
+				 labelArray[r][c].setBorder(BorderFactory.createLineBorder(new Color(0,0,0)));
 				  
-				  if(r==4 && c==3)
-					  label.setIcon(createAlien(r,c,"west"));
-			  }
+				  
+				 if (theWorld.getLifeForm(r, c) != null)
+					 labelArray[r][c].setIcon(createLifeForm(theWorld.getLifeForm(r, c), r,c,"north"));
+				 else
+					 labelArray[r][c].setIcon(createSpace(r,c));
+				 centerPanel.add(labelArray[r][c]);
+			 }
 		 }
+		 
 		 add("Center",centerPanel);
 		 pack();
 		 setVisible(true);
 
 	}
-	public ImageIcon createImage()
+	
+	public ImageIcon createLifeForm(LifeForm l, int x, int y, String dir)
 	{
-		 BufferedImage exampleImage = new
-		 BufferedImage(50,50,BufferedImage.TYPE_3BYTE_BGR);
-		 Graphics drawer = exampleImage.getGraphics();
-	
-		 drawer.setColor(new Color(200,200,200));
-		 drawer.fillRect(0, 0, 50, 50);
-	
-		 drawer.setColor(new Color(0,255,0));
-		 drawer.fillOval(20, 20, 10, 10);
-	
-		 return new ImageIcon(exampleImage);
-	 } 
-	
-	public ImageIcon createAlien(int x, int y, String dir)
-	{
-		BufferedImage alien = new
+		BufferedImage lifeform = new
 		BufferedImage(50,50,BufferedImage.TYPE_3BYTE_BGR);
-		Graphics drawer = alien.getGraphics();
-					
+		Graphics drawer = lifeform.getGraphics();
+							
 		drawer.setColor(new Color(255,255,255));
 		drawer.fillRect(x, y, 50, 50);
-		drawer.setColor(new Color(248,206,204));
+		if (l instanceof  Alien)
+			drawer.setColor(new Color(248,206,204));
+		else if (l instanceof  Human)
+			drawer.setColor(new Color(218,232,252));
 		drawer.fillOval(0, 0, 50, 50);
 		drawer.setColor(new Color(200,200,200));
 		//Color is direction they're facing
@@ -123,34 +106,18 @@ public class UI extends JFrame{
 		//Facing South
 		else if (dir == "south")
 			drawer.fillArc(0, 0, 50, 50, 180, -180);
-		return new ImageIcon(alien);		
+		return new ImageIcon(lifeform);			
 	}
 	
-	public ImageIcon createHuman(int x, int y, String dir)
+	public ImageIcon createSpace(int x, int y)
 	{
-		BufferedImage human = new
+		BufferedImage space = new
 		BufferedImage(50,50,BufferedImage.TYPE_3BYTE_BGR);
-		Graphics drawer = human.getGraphics();
+		Graphics drawer = space.getGraphics();
+									
 		drawer.setColor(new Color(255,255,255));
 		drawer.fillRect(x, y, 50, 50);
-		drawer.setColor(new Color(218,232,252));
-		drawer.fillOval(0, 0, 50, 50);
-		drawer.setColor(new Color(200,200,200));
-		//Color is direction they're facing
-		//Facing West
-		if (dir == "west")
-			drawer.fillArc(0, 0, 50, 50, 90, -180);
-		//Facing East
-		else if (dir == "east")
-			drawer.fillArc(0, 0, 50, 50, 90, 180);
-		//Facing North
-		else if (dir == "north")
-			drawer.fillArc(0, 0, 50, 50, -180, 180);
-		//Facing South
-		else if (dir == "south")
-			drawer.fillArc(0, 0, 50, 50, 180, -180);
-		
-		return new ImageIcon(human);		
+		return new ImageIcon(space);		
 	}
 }
 
--- Fulton_Lab2/test/ui/TestUI.java
+++ Fulton_Lab2/test/ui/TestUI.java
@@ -6,19 +6,46 @@ import javax.swing.JOptionPane;
 
 import org.junit.Test;
 
+import environment.Cell;
 import environment.Environment;
+import exceptions.ExistingWorldException;
+import lifeform.Alien;
+import lifeform.Human;
+
 
 public class TestUI {
 
+	
 	@Test
-	public void testWorldGridInitialization() {
-		UI ui = new UI();
+	public void testWorldGridInitialization() throws ExistingWorldException 
+	{
+		Environment.clearBoard();
+		Environment.setupWorld(10,10);
+		Environment e = Environment.getWorld();
+		UI ui = new UI(e);
+		//UI ui = new UI();
+		
+		assertEquals(JOptionPane.YES_OPTION,JOptionPane.showConfirmDialog(null, 
+				"Does the grid look right?"));
+	
+	}
+	
+	@Test
+	public void testWorldGridHumanAlienLocations() throws ExistingWorldException {
+		Environment.clearBoard();
+		Environment.setupWorld(10,10);
+		Environment e = Environment.getWorld();
+		Human bob = new Human("Bob", 10, 10);
+		Alien aBob = new Alien("AlienBob", 10);
+		e.addLifeForm(bob, 0, 0);
+		e.addLifeForm(aBob, 4, 3);
+		UI ui = new UI(e);
 		
 		assertEquals(JOptionPane.YES_OPTION,JOptionPane.showConfirmDialog(null, 
 				"Create Cell Image Icon Correct For\nHuman(0,0) facing North\nDoes it look right?"));
 	
 		assertEquals(JOptionPane.YES_OPTION,JOptionPane.showConfirmDialog(null, 
-				"Create Cell Image Icon Correct For\nAlien(4,3) facing West\nDoes it look right?"));
+				"Create Cell Image Icon Correct For\nAlien(4,3) facing North\nDoes it look right?"));
 	}
 
 }
